759a6198e60dbcd91c9581b3c272b9f095e0c2de
Kaloian Ianev <kianev@gmail.com>
2017-12-10 08:44:12 +0200

    Routing added to the project

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,34 +91,12 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true})
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Pesho',
-        address: {
-          street: 'TestStreet 1',
-          city: 'Sofia',
-          country: 'Bulgaria'
-        },
-        email: 'test@email.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-     axios.post('/orders.json', order)
-       .then(response => {
-         this.setState({loading: false, purchasing: false})
-       })
-       .catch(error => {
-         this.setState({loading: false, purchasing: false})
-       });
-
     const queryparams = [];
     for(let i in this.state.ingredients){
       queryparams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
 
+    queryparams.push('price=' + this.state.totalPrice);
     const queryString = queryparams.join('&');
 
     this.props.history.push({
